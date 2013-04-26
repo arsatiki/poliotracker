fa310813de822528c60b7558d0275a2f2d5d3c74
Antti Rasinen <antti.rasinen@codento.com>
2013-04-26 11:13:58 +0300

    Refactor screenscraping to a separate function

--- scrub.py
+++ scrub.py
@@ -3,16 +3,17 @@ import requests
 
 SOURCE = 'http://www.polioeradication.org/Dataandmonitoring/Poliothisweek.aspx'
 
-def main():
-    r = requests.get(SOURCE)
-    doc = BeautifulSoup(r.text)
+def parsepage(text):
+    doc = BeautifulSoup(text)
     casetable = doc.findAll('table')[1]
     countries = casetable.findAll('tr')[2:-3]
     for c in countries:
         cells = c.findAll('td', text=True)
-        country_name = cells[1]
-        last_case = cells[-1]
-        print country_name, last_case
+        yield cells[1], cells[-1]
+
+def main():
+    r = requests.get(SOURCE)
+    countries = parsepage(r.text)
     
 if __name__ == '__main__':
     main()
