043d291fd5057dac722a49b5b4a60dae5cbe674f
Antti Rasinen <antti.rasinen@codento.com>
2013-06-06 22:04:04 +0300

    Fix parsing errors

--- scrub.py
+++ scrub.py
@@ -5,17 +5,27 @@ import requests
 SOURCE = 'http://www.polioeradication.org/Dataandmonitoring/Poliothisweek.aspx'
 
 def parsepage(text):
-    doc = BeautifulSoup(text)
+    doc = BeautifulSoup(text, convertEntities="html")
     casetable = doc.findAll('table')[1]
     countries = casetable.findAll('tr')[2:-3]
     for c in countries:
         cells = c.findAll('td', text=True)
-        yield cells[1], cells[-1]
+        while not cells[0].strip():
+            cells.pop(0)
+        while not cells[-1].strip():
+            cells.pop(-1)
+        yield cells[0], cells[-1]
+
+def parsedate(d):
+    try:
+        return datetime.strptime(d, '%d-%b-%y')
+    except ValueError:
+        return datetime.strptime(d, '%d-%b-%Y')
 
 def main():
     r = requests.get(SOURCE)
     countries = parsepage(r.text)
-    dates = (datetime.strptime(d, '%d-%b-%y') for (c, d) in countries)
+    dates = (parsedate(d) for (c, d) in countries)
     diff = date.today() - max(dates).date()
     print date.today(), diff.days
     
