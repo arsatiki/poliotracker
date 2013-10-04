d3c4b079ad9e2c3696cb29d0ed88c28cf5d1aa88
Antti Rasinen <antti.rasinen@codento.com>
2013-10-04 16:01:39 +0300

    Add days since last case to stathat

--- scrub.py
+++ scrub.py
@@ -1,6 +1,7 @@
 from BeautifulSoup import BeautifulSoup
 from datetime import datetime, date
 import requests
+from stathat import StatHat
 
 SOURCE = 'http://www.polioeradication.org/Dataandmonitoring/Poliothisweek.aspx'
 
@@ -17,16 +18,21 @@ def parsepage(text):
         yield cells[0], cells[-1]
 
 def parsedate(d):
-    try:
-        return datetime.strptime(d, '%d-%b-%y')
-    except ValueError:
-        return datetime.strptime(d, '%d-%b-%Y')
+    formats = ('%d-%b-%y', '%d-%b-%Y', '%d -%b-%y' )
+    for f in formats:
+        try:
+            return datetime.strptime(d, f)
+        except ValueError:
+            continue
+    return None
 
 def main():
+    stats = StatHat()
     r = requests.get(SOURCE)
     countries = parsepage(r.text)
-    dates = (parsedate(d) for (c, d) in countries)
+    dates = filter(None, (parsedate(d) for (c, d) in countries))
     diff = date.today() - max(dates).date()
+    stats.ez_post_value("site-stathat.com@ars.iki.fi", "days since last case", diff.days)
     print date.today(), diff.days
     
 if __name__ == '__main__':
