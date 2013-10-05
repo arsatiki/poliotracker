97ed7d4be92e8e25b065358f1194c07228f6ca1e
Antti Rasinen <antti.rasinen@codento.com>
2013-10-05 13:12:09 +0300

    Add support for tracking case counts

--- scrub.py
+++ scrub.py
@@ -7,8 +7,7 @@ import sys
 NO_UPLOAD = False
 FORCE_FETCH = False
 
-def parsepage(text):
-    doc = BeautifulSoup(text, convertEntities="html")
+def find_countries(doc):
     casetable = doc.findAll('table')[1]
     countries = casetable.findAll('tr')[2:-3]
     for c in countries:
@@ -18,6 +17,17 @@ def parsepage(text):
         while not cells[-1].strip():
             cells.pop(-1)
         yield cells[0], cells[-1]
+
+def find_counts(doc):
+    def at(cell, x, y):
+        return int(cell[x + 4 * y])
+    
+    cell = [c for c in doc.table(text=True) if c.strip()]
+
+    this_year = (at(cell, 1, 1), at(cell, 1, 2), at(cell, 1, 3))
+    last_year = (at(cell, 2, 1), at(cell, 2, 2), at(cell, 2, 3))
+
+    return this_year, last_year
 
 def parsedate(d):
     formats = ('%d-%b-%y', '%d-%b-%Y', '%d -%b-%y' )
@@ -50,11 +60,19 @@ def main():
     text, modified = fetch()
     if not (modified or FORCE_FETCH):
         sys.exit()
-        
-    countries = parsepage(text)
+    
+    doc = BeautifulSoup(text, convertEntities="html")
+    
+    countries = find_countries(doc)
     dates = filter(None, (parsedate(d) for (c, d) in countries))
     diff = date.today() - max(dates).date()
-    publish("time since last case", diff.days)
+    #publish("time since last case", diff.days)
+    
+    this_year, last_year = find_counts(doc)
+    publish("cases globally this year", this_year[0])
+    publish("cases in endemic countries this year", this_year[1])
+    publish("cases globally last year", last_year[0])
+    publish("cases in endemic countries last year", last_year[1])
     
 if __name__ == '__main__':
     main()
