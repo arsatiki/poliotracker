56de69a46e92f27734a946f9f38ff3ced2d643f4
Antti Rasinen <antti.rasinen@codento.com>
2013-12-05 13:10:58 +0200

    Reorganize file names

---
+++ parse.py
@@ -0,0 +1,58 @@
+from datetime import datetime, date
+import csv
+import sys
+import time
+
+def find_countries(doc):
+    casetable = doc.findAll('table')[1]
+    countries = casetable.findAll('tr')[2:-3]
+    for c in countries:
+        cells = c.findAll('td', text=True)
+        while not cells[0].strip():
+            cells.pop(0)
+        while not cells[-1].strip():
+            cells.pop(-1)
+        yield cells[0], cells[-1]
+
+def find_counts(doc):
+    def at(cell, x, y):
+        return int(cell[x + 4 * y])
+    
+    cell = [c for c in doc.table(text=True) if c.strip()]
+
+    this_year = (at(cell, 1, 1), at(cell, 1, 2), at(cell, 1, 3))
+    last_year = (at(cell, 2, 1), at(cell, 2, 2), at(cell, 2, 3))
+
+    return this_year, last_year
+
+def parsedate(d):
+    formats = ('%d-%b-%y', '%d-%b-%Y', '%d -%b-%y' )
+    for f in formats:
+        try:
+            return datetime.strptime(d, f)
+        except ValueError:
+            continue
+    return None
+
+def publish(endemic, non_endemic, days_since_last=None, last_year=False):
+    timestamp = int(time.time())
+    if last_year:
+        timestamp -= 365 * 24 * 60 * 60
+
+    if days_since_last is not None:
+        print "%d\t%d\t%d\t%d" % (timestamp, endemic, non_endemic, days_since_last)
+    else:
+        print "%d\t%d\t%d\tNaN" % (timestamp, endemic, non_endemic)
+
+
+def main():
+    countries = find_countries(doc)
+    dates = filter(None, (parsedate(d) for (c, d) in countries))
+    diff = date.today() - max(dates).date()
+    
+    this_year, last_year = find_counts(doc)
+    publish(this_year[1], this_year[2], days_since_last=diff.days)
+    publish(last_year[1], last_year[2], last_year=True)
+    
+if __name__ == '__main__':
+    main()
--- scrub.py
+++ scrub.py
@@ -1,58 +1,43 @@
-from datetime import datetime, date
+# encoding: utf-8
 import csv
 import sys
-import time
+from BeautifulSoup import BeautifulSoup
+from fetcher import fetch
 
-def find_countries(doc):
+EXIT_UNMODIFIED = 1
+FORCE_FETCH = False
+
+def write(doc, w):
     casetable = doc.findAll('table')[1]
-    countries = casetable.findAll('tr')[2:-3]
-    for c in countries:
-        cells = c.findAll('td', text=True)
-        while not cells[0].strip():
-            cells.pop(0)
-        while not cells[-1].strip():
-            cells.pop(-1)
-        yield cells[0], cells[-1]
+    countries = casetable.findAll('tr')[2:]
+    for country in countries:
+        cells = (c.text for c in country.findAll('td'))
+        cleaned = (c.strip() for c in cells)
+        row = [c.encode('utf-8') for c in cleaned]
+        w.writerow(row)
 
-def find_counts(doc):
-    def at(cell, x, y):
-        return int(cell[x + 4 * y])
-    
-    cell = [c for c in doc.table(text=True) if c.strip()]
-
-    this_year = (at(cell, 1, 1), at(cell, 1, 2), at(cell, 1, 3))
-    last_year = (at(cell, 2, 1), at(cell, 2, 2), at(cell, 2, 3))
-
-    return this_year, last_year
-
-def parsedate(d):
-    formats = ('%d-%b-%y', '%d-%b-%Y', '%d -%b-%y' )
-    for f in formats:
-        try:
-            return datetime.strptime(d, f)
-        except ValueError:
-            continue
-    return None
-
-def publish(endemic, non_endemic, days_since_last=None, last_year=False):
-    timestamp = int(time.time())
-    if last_year:
-        timestamp -= 365 * 24 * 60 * 60
-
-    if days_since_last is not None:
-        print "%d\t%d\t%d\t%d" % (timestamp, endemic, non_endemic, days_since_last)
-    else:
-        print "%d\t%d\t%d\tNaN" % (timestamp, endemic, non_endemic)
-
+def parse_opts():
+    global FORCE_FETCH
+    if len(sys.argv) > 1:
+        for opt in sys.argv[1:]:
+            if opt == '-f':
+                FORCE_FETCH = True
 
 def main():
-    countries = find_countries(doc)
-    dates = filter(None, (parsedate(d) for (c, d) in countries))
-    diff = date.today() - max(dates).date()
+    parse_opts()
+    text, modified = fetch()
+    if not (modified or FORCE_FETCH):
+        print "Not modified, skipping"
+        sys.exit(EXIT_UNMODIFIED)
     
-    this_year, last_year = find_counts(doc)
-    publish(this_year[1], this_year[2], days_since_last=diff.days)
-    publish(last_year[1], last_year[2], last_year=True)
+    doc = BeautifulSoup(text, convertEntities="html")
+    w = csv.writer(sys.stdout, delimiter="\t")
+    #TODO: Move this out.
+    header = ("Country", "WPV1-cy2d", "WPV3-cy2d", "W1W3-cy2d", "Total-cy2d",
+                         "WPV1-py2d", "WPV3-py2d", "W1W3-py2d", "Total-py2d",
+                         "Total-pyf", "Date of most recent")
+    w.writerow(header)
+    write(doc, w)
     
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
