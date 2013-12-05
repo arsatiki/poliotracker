1274c4f50c16303758946ef924cca306f852ac92
Antti Rasinen <antti.rasinen@codento.com>
2013-12-05 13:54:35 +0200

    Add parser for days since last

--- parse.py
+++ parse.py
@@ -1,7 +1,10 @@
 from datetime import datetime, date
 import csv
+from itertools import islice, imap, ifilter
 import sys
 import time
+
+TODAY = str(date.today())
 
 def find_countries(doc):
     casetable = doc.findAll('table')[1]
@@ -25,16 +28,8 @@ def find_counts(doc):
 
     return this_year, last_year
 
-def parsedate(d):
-    formats = ('%d-%b-%y', '%d-%b-%Y', '%d -%b-%y' )
-    for f in formats:
-        try:
-            return datetime.strptime(d, f)
-        except ValueError:
-            continue
-    return None
 
-def publish(endemic, non_endemic, days_since_last=None, last_year=False):
+def opublish(endemic, non_endemic, days_since_last=None, last_year=False):
     timestamp = int(time.time())
     if last_year:
         timestamp -= 365 * 24 * 60 * 60
@@ -44,8 +39,50 @@ def publish(endemic, non_endemic, days_since_last=None, last_year=False):
     else:
         print "%d\t%d\t%d\tNaN" % (timestamp, endemic, non_endemic)
 
+def parsedate(d):
+    formats = ('%d-%b-%y', '%d-%b-%Y', '%d -%b-%y' )
+    for f in formats:
+        try:
+            return datetime.strptime(d, f)
+        except ValueError:
+            continue
+    return None
+
+def publish(out, *items):
+    out.writerow([TODAY] + list(items))
+
+def days(data, out):
+    rows = list(data)
+    country_rows = rows[:-3]
+    datestrings = (row['Date of most recent'] for row in country_rows)
+    dates = ifilter(None, imap(parsedate, datestrings))
+
+    diff = date.today() - max(dates).date()
+    publish(out, diff.days)
+
+PARSER = {
+    'days': days,
+    #'cases': cases,
+    #'countries': countries
+}
 
 def main():
+    if len(sys.argv) != 2:
+        print >> sys.stdout, "Expected parse command" 
+        sys.exit(1)
+    
+    subcommand = sys.argv[1]
+    
+    if subcommand not in PARSER:
+        print >> sys.stdout, "Incorrect parse command:", subcommand
+        sys.exit(1)
+    
+    data = csv.DictReader(sys.stdin, delimiter='\t')
+    out = csv.writer(sys.stdout, delimiter='\t')
+    
+    PARSER[subcommand](data, out)
+    
+def oldmain():
     countries = find_countries(doc)
     dates = filter(None, (parsedate(d) for (c, d) in countries))
     diff = date.today() - max(dates).date()
@@ -53,6 +90,8 @@ def main():
     this_year, last_year = find_counts(doc)
     publish(this_year[1], this_year[2], days_since_last=diff.days)
     publish(last_year[1], last_year[2], last_year=True)
+    
+
     
 if __name__ == '__main__':
     main()
